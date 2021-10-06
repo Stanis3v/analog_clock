8fb1f40fe473f670575e3de35ab052bee2c06c0e
Stanis <stanisvel.dev@gmail.com>
2021-10-06 20:55:18 +0200

    Structure

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AppState();
+}
+class _AppState extends State<App> with WidgetsBindingObserver {
+
+}
